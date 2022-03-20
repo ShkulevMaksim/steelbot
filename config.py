1269eb5e5075c47d06e768371c28f9bdc15600b8
maxim <mksh22@ya.ru>
2022-03-21 01:47:55 +0700

    021

---
+++ config.py
@@ -0,0 +1,20 @@
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+ffmpeg_options = {
+    'options': '-vn'
+}
+settings = {
+    'prefix': '?',
+    'token' : 'ODk3MzkwNzU2MDc4MTgyNDAw.YWU-RA.DHCNVa9PGAavE1hQdgKGmXlPtAo'
+}
